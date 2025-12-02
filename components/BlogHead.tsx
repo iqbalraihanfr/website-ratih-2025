e5e5e383d24311dc964448fd0afd219e0ddd3a35
AfifSatrio <afifsatria2108@gmail.com>
2025-12-02 23:28:47 +0700

    Optimilize UI at BlogHead

---
+++ components/BlogHead.tsx
@@ -0,0 +1,12 @@
+const BlogHead = () => {
+  return (
+    <div className="bg-[linear-gradient(to_bottom,rgba(0,0,0,0.1),rgba(0,0,0,0.9)),url('/images/bg/bg-2.png')] bg-no-repeat bg-cover bg-center py-10 px-10 h-[50vh] flex flex-col items-center justify-center text-white transition-all">
+      <h1 className="text-sm font-medium uppercase pt-10">
+            Tambah Wawasanmu Bersama
+      </h1>
+      <p className="text-4xl lg:text-5xl font-bold italic">BERITA RATIH</p>
+    </div>
+  )
+}
+
+export default BlogHead
