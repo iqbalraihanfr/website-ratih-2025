4d97e896f26cd21837fa30edc4ff484e3debf80b
AfifSatrio <afifsatria2108@gmail.com>
2026-01-03 23:06:59 +0700

    update changes

--- components/AboutContent.tsx
+++ components/AboutContent.tsx
@@ -1,15 +1,11 @@
-import AboutRatih from "./AboutRatih"
-import LogoRatih from "./LogoRatih"
+import AllAboutRatih from "./AllAboutRatih"
 import RATIH from "./RATIH"
 import RatihCrew from "./RatihCrew"
 
 const AboutContent = () => {
   return (
     <div className="container mx-auto transition-all">
-      <div className="flex flex-col lg:flex-row mt-20 pb-10 lg:mx-20 mx-10 items-center">
-        <LogoRatih />
-        <AboutRatih />
-      </div>
+      <AllAboutRatih />
       <RATIH />
       <RatihCrew />
     </div>
---
+++ components/AllAboutRatih.tsx
@@ -0,0 +1,13 @@
+import LogoRatih from './LogoRatih'
+import AboutRatih from './AboutRatih'
+
+const AllAboutRatih = () => {
+  return (
+    <div className="flex flex-col lg:flex-row mt-20 pb-10 lg:mx-20 mx-10 items-center">
+        <LogoRatih />
+        <AboutRatih />
+    </div>
+  )
+}
+
+export default AllAboutRatih
